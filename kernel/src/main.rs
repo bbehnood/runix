44654ddcbab1934304e461f8deff97f337ed95d5
Behnood Abdollahi <behnood.abdollahi2011@gmail.com>
2026-08-14 19:38:55 +0330

    Initial project setup

---
+++ kernel/src/main.rs
@@ -0,0 +1,17 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+use bootloader_api::{BootInfo, entry_point};
+
+entry_point!(kernel_main);
+
+fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
